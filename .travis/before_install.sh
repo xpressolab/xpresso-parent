0b5f10134cf58eb16e33afa6deb7d2bf41813e6a
Felipe Santos <felipemsantos@gmail.com>
2017-09-27 22:34:15 -0300

    Nova configuracao do travis

--- .travis/before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "$COMMIT_AUTHOR_NAME"
-git config --global user.email "$COMMIT_AUTHOR_EMAIL"
-
-git config --global user.name
-git config --global user.email
-
-export COMMIT_MESSAGE=`git log -1 --oneline`
